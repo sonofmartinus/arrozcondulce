22daed3a3972d37915c5967ac04b80257624612a
sonofmartinus <git@sonofmartinus.com>
2025-01-19 21:15:37 -0600

    feat: adding semver and auto releases

---
+++ release.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	branches: ["main"],
+	plugins: [
+	  "@semantic-release/commit-analyzer",
+	  "@semantic-release/release-notes-generator",
+	  "@semantic-release/changelog",
+	  [
+		"@semantic-release/npm",
+		{
+		  npmPublish: false,
+		},
+	  ],
+	  [
+		"@semantic-release/github",
+		{
+		  assets: ["CHANGELOG.md"],
+		},
+	  ],
+	],
+  };
