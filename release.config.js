2558bff6ce4514e6c5b72408c0246e0cd03a4fd7
sonofmartinus <git@sonofmartinus.com>
2025-02-01 18:24:13 -0600

    fix: remove some of the semver bloat

--- release.config.js
+++ release.config.js
@@ -1,27 +1,14 @@
 module.exports = {
 	branches: ["main"],
 	plugins: [
-	  "@semantic-release/commit-analyzer", // Determines the type of version bump (major, minor, patch)
-	  "@semantic-release/release-notes-generator", // Generates release notes
-	  "@semantic-release/changelog", // Updates the CHANGELOG.md
-	  [
-		"@semantic-release/npm",
-		{
-		  npmPublish: false, // Prevents publishing to npm, only updates package.json and package-lock.json
-		},
-	  ],
+	  "@semantic-release/commit-analyzer",
+	  "@semantic-release/release-notes-generator",
+	  "@semantic-release/changelog",
 	  [
 		"@semantic-release/github",
 		{
 		  assets: ["CHANGELOG.md"], // Upload changelog to GitHub release
 		},
-	  ],
-	  [
-		"@semantic-release/git",
-		{
-		  assets: ["package.json", "package-lock.json"], // Commit updated package.json and lockfile
-		  message: "chore(release): bump version to ${nextRelease.version} [skip ci]",
-		},
-	  ],
+	  ]
 	],
-  };
+};
